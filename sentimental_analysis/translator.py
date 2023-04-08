e0658afe6d850ef65692d3d8b7f55c83195ce822
huangj20 <huangj20@myumanitoba.ca>
2023-04-08 12:23:11 -0700

    merge all csv files

--- sentimental_analysis/translator.py
+++ sentimental_analysis/translator.py
@@ -1,40 +1,42 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf, col
+from pyspark.sql.functions import udf
 from pyspark.sql.types import StringType
 from googletrans import Translator
+import col
 
-import sys, re
+import sys
 
 def main():
     # Read the CSV file into a PySpark DataFrame
-    df = spark.read.csv("sample.csv", header=True)
-    text_col = col("Official ManCity team news for MCIBOU")
+    df = spark.read.csv('twitter_news.csv', header=True)
+    df = df.withColumnRenamed(columns={'Official   ManCity team news for  MCIBOU': 'Official ManCity team news for MCIBOU'}, inplace=True)
+    text_col = col('Official ManCity team news for MCIBOU')
     
     translate_udf = udf(translate_text, StringType())
     
     # Apply the translation UDF to the DataFrame
-    df = df.withColumn("translated_text", translate_udf(df['text']))
+    df = df.withColumn('translated_text', translate_udf(text_col))
 
     # Write the translated data to a new file
-    df.write.format("csv").option("header", "true").mode("overwrite").save("output_trans.csv")
+    df.write.format('csv').option('header', 'true').mode('overwrite').save('output_trans.csv')
 
 
 
 # Define a translation function as a UDF
 def translate_text(text):
-    translator = Translator(service_urls=['translate.google.com']) 
-    text = re.sub("[^a-zA-Z]", " ", text)
+    translator = Translator()
+    translator.raise_Exception = True
     try:
-        translated_text = translator.translate(text, dest='en').text
-        return translated_text
-    except Exception as e:
-        print(f"Translation failed: {e}")
-        return ""
+        trans_obj = translator.translate(text, 'en')
+    except Exception as err:
+        translator.raise_Exception = True
+        trans_obj = translator.translate(text, 'en')
+    return trans_obj.text
 
 
 if __name__ == '__main__':
     # Create a SparkSession
-    spark = SparkSession.builder.appName("TranslationApp").getOrCreate()    
+    spark = SparkSession.builder.appName('TranslationApp').getOrCreate()    
     assert sys.version_info >= (3, 5) # make sure we have Python 3.5+  
     spark.sparkContext.setLogLevel('WARN')
     sc = spark.sparkContext
---
+++ streamlit_graphs/data_preparation.py
@@ -0,0 +1,23 @@
+# data preparation for graphs
+# Path: streamlit_graphs/data_preparation.py
+import pandas as pd
+import glob
+
+tag_name = 'news'
+# integrate the data from all csv files
+folder_path = f"/Users/xxxibdara/Downloads/733-final-project/scraping/{tag_name}"
+
+# read all csv files in the folder
+all_files = glob.glob(folder_path + "/*.csv")
+
+# save them into a dataframes
+df_list = []
+
+for file_path in all_files:
+    df = pd.read_csv(file_path)
+    df_list.append(df)
+
+all_data = pd.concat(df_list, axis=0, ignore_index=True)
+
+# save the data into a csv file
+all_data.to_csv(f'{tag_name}_all.csv', index=False)
