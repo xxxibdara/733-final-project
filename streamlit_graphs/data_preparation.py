e8eeafb007e010c937d6a21175c04e33416fe20e
huangj20 <huangj20@myumanitoba.ca>
2023-04-08 21:00:20 -0700

    create graphs

--- streamlit_graphs/data_preparation.py
+++ streamlit_graphs/data_preparation.py
@@ -1,31 +1,24 @@
 # data preparation for graphs
-from pyspark.sql import SparkSession
-import sys, glob
+# Path: streamlit_graphs/data_preparation.py
+import pandas as pd
+import glob
 
-def main(input):
-    # get the tag name
-    tag_name = f'{input}'
-    # integrate the data from all csv files
-    folder_path = f"/Users/xxxibdara/Downloads/733-final-project/scraping/{tag_name}"
+tag_name = 'covid'
+# integrate the data from all csv files
+folder_path = f"/Users/xxxibdara/Downloads/733-final-project/scraping/{tag_name}"
 
-    # read all csv files in the folder
-    all_files = glob.glob(folder_path + "/*.csv")
+# read all csv files in the folder
+all_files = glob.glob(folder_path + "/*.csv")
 
-    # create an empty dataframe to store the data
-    df = spark.createDataFrame([], schema="tweet_id long, user string, text string, num_comments int, num_retweets int, num_views string, timestamp timestamp")
+# save them into a dataframes
+df_list = []
 
-    # read all files and append them to the dataframe
-    for file_path in all_files:
-        file_df = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load(file_path)
-        df = df.union(file_df)
+for file_path in all_files:
+    df = pd.read_csv(file_path)
+    df_list.append(df)
 
-    # write the dataframe to a CSV file
-    df.write.format("csv").mode("overwrite").option("header", "true").option("timestampFormat", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").save(f"{folder_path}_all.csv")
+all_data = pd.concat(df_list, axis=0, ignore_index=True)
 
-if __name__ == '__main__':
-    spark = SparkSession.builder.appName('data preparation').getOrCreate()
-    assert spark.version >= '3.0' # make sure we have Spark 3.0+
-    spark.sparkContext.setLogLevel('WARN')
-    sc = spark.sparkContext
-    inputs = sys.argv[1]
-    main(inputs)
+# save the data into dataframe
+df = pd.DataFrame(all_data)
+df.to_csv(f"{tag_name}.csv", index=False)
---
+++ streamlit_graphs/views_graph.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+# set the page title
+st.title('Twitter data changes over time')
+
+# set the page subtitle
+st.subheader('This app displays the number of views, retweets, and comments for a given tag over time')
+
+# create a sidebar for entering the tag name
+st.sidebar.write('Our TOP 10 tags are: covid, news, ')
+tag = st.sidebar.text_input('Enter the tag name from our TOP10 list','covid')
+
+# load data
+df = pd.read_csv(f'{tag}.csv')
+
+# convert timestamp column to datetime format, handling errors gracefully
+df['timestamp'] = pd.to_datetime(df['timestamp'], errors='coerce')
+
+# drop rows with NaN values
+df = df.dropna(subset=['timestamp', 'num_views', 'num_retweets', 'num_comments'])
+
+# create a function to display the chart
+def display_chart(metric):
+    fig = px.bar(filtered_df, x='timestamp', y=metric, title=f'{metric} over time')
+    fig.update_layout(margin=dict(l=20, r=20, t=40, b=20),
+    width=800,
+    height=400)
+    st.plotly_chart(fig)
+
+# create a sidebar for selecting the metric to display
+st.sidebar.title('Select a metric to display')
+metric = st.sidebar.selectbox('Metric:', ['num_views', 'num_retweets', 'num_comments'])
+
+# create a date range slider for filtering the data by date
+start_date, end_date = st.sidebar.date_input('Date range:', [df['timestamp'].min(), df['timestamp'].max()], min_value=df['timestamp'].min(), max_value=df['timestamp'].max())
+start_date = pd.Timestamp(start_date, tz='UTC')
+end_date = pd.Timestamp(end_date, tz='UTC')
+filtered_df = df[(df['timestamp'] >= start_date) & (df['timestamp'] <= end_date)]
+
+
+# display the chart based on the selected metric and date range
+display_chart(metric)
