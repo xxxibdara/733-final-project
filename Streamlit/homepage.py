5e67f1ed6471f3ddbffeafc2ebf54a781ba1fff4
Xiner Qian <69947529+qian-x2@users.noreply.github.com>
2023-04-10 11:21:51 -0700

    Edit streamlit requirement

---
+++ Streamlit/homepage.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Building a Popular Tweet: Tag Prediction and Sentiment Analysis")
+
+st.subheader("Our motivation and Background")
+
+st.write("Social media platforms like Twitter have become one of the most \
+          popular sources of information and communication, especially among \
+          young people. Millions of tweets are posted every day on various \
+         topics ranging from news, politics, sports, entertainment, and more. \
+         These tweets contain a wealth of information, including people's \
+         opinions, thoughts, and sentiments about various topics.")
