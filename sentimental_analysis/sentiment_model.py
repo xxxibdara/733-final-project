a2685fec8d7f7abf5f017fde1b8a950c5437be4c
huangj20 <huangj20@myumanitoba.ca>
2023-04-11 10:38:24 -0700

    slightly change

--- sentimental_analysis/sentiment_model.py
+++ sentimental_analysis/sentiment_model.py
@@ -1,62 +1,28 @@
-#!/usr/bin/env python
-# coding: utf-8
+# Description: This file contains the code for the sentiment model
 
-# In[1]:
-
-
-# Importing needed libraries
-import numpy as np
+# importing needed libraries
 import pandas as pd
-from datetime import datetime
-import seaborn as sns
-import matplotlib.pyplot as plt
-get_ipython().run_line_magic('matplotlib', 'inline')
-from collections import Counter
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from textblob import TextBlob
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.metrics import mean_squared_error, accuracy_score, precision_score, recall_score, f1_score
-from sklearn.model_selection import train_test_split
-import scipy.sparse as sp
+import streamlit as st
 
 # NLTK tools for text processing
 import re, nltk
 from nltk import word_tokenize
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-from nltk import FreqDist
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-from wordcloud import STOPWORDS
-from wordcloud import WordCloud
-nltk.download('stopwords')
 nltk.download('punkt')
-nltk.download('vader_lexicon')
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from nltk.tokenize import word_tokenize
 
-
-# In[2]:
-
-
-df = pd.read_csv('df_clean.csv')
-
-
-# In[3]:
-
+tag = st.text_input("Enter the tag you want to search for: ", 'covid')
+df = pd.read_csv(f'{tag}_clean.csv')
 
 tweet_text = df['text']
 sid = SentimentIntensityAnalyzer()
 
-
-# In[4]:
-
-
 df['compound'] = tweet_text.apply(sid.polarity_scores)
-
-
-# In[5]:
-
 
 extract_values = lambda x: pd.Series([x['neg'], x['neu'], x['pos'], x['compound']], 
                                      index=['neg', 'neu', 'pos', 'compound'])
@@ -65,33 +31,15 @@ extract_values = lambda x: pd.Series([x['neg'], x['neu'], x['pos'], x['compound'
 df[['neg', 'neu', 'pos', 'compound']] = df['compound'].apply(extract_values)
 df.dropna(subset=['compound'], inplace=True)
 
-
-# In[9]:
-
-
 def detect_sentiment(text):
     blob = TextBlob(text)
     return blob.sentiment.polarity
 df['sentiment'] = tweet_text.apply(detect_sentiment)
 
-
-# In[11]:
-
-
 df['num_comments'] = df['num_comments'].astype(float)
 df['num_retweets'] = df['num_retweets'].astype(float)
 df['num_views'] = df['num_views'].astype(float)
 df['timestamp'] = pd.to_datetime(df['timestamp'])
-
-
-# In[12]:
-
-
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.model_selection import train_test_split
-from nltk.tokenize import word_tokenize
-from sklearn.feature_extraction.text import CountVectorizer
 
 df['text_tokens'] = df['text'].apply(word_tokenize)
 vectorizer = CountVectorizer()
@@ -120,9 +68,7 @@ mse = mean_squared_error(y_test, y_pred)
 print("MSE:", mse)
 
 
-# In[22]:
-
-
+# test our model
 text_input = "In today's Tech Friend: Everyone can improve online security with these password tips. But the whole password system is dumb, insecure and sets us up to fail. Our long term mission: Kill passwords forever. It's possible!."
 num_comments = 100
 num_retweets = 500
@@ -139,8 +85,6 @@ y_input_pred = rf_model.predict(X_input)
 
 print("Predicted compound score:", y_input_pred)
 
-
-# In[ ]:
 
 
 
